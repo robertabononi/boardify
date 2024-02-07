17e5ae7946aa5cbac0cceb8505fadc9ed43030c1
Roberta Bononi da Silva <roberta.silva@ibm.com>
2024-02-07 09:34:32 -0300

    feat: criação componentes para base e active tasks

---
+++ src/app/active-tasks/active-tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './active-tasks.component.html',
+  styleUrl: './active-tasks.component.scss'
+})
+export class ActiveTasksComponent {
+
+}
